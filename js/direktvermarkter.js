1b74f820851e3f921951d9ae239123fb83eebcf6
Stefan Grotz <stefan.grotz@posteo.de>
2018-03-15 19:58:27 +0100

    hyperlinks und darstellung

--- js/direktvermarkter.js
+++ js/direktvermarkter.js
@@ -15,19 +15,31 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //Daten
 
 function jederPunkt (feature, layer){
- var popupcontent = [];
-    for (var prop in feature.properties) {
-        popupcontent.push("<tr><td>" +prop.replace(":", " ").replace("addr ", "").replace("name", "Name").replace("opening_hours", "Öffnungszeiten").replace("city", "Stadt").replace("housenumber", "Hausnummer").replace("phone", "Telefon").replace("operator", "Betreiber").replace("postcode", "Postleitzahl").replace("street", "Straße").replace("organic", "Biologisch") + ": </td><td>" + feature.properties[prop].replace(";", ", ").replace("yes", "ja").replace("only", "nur") + "</td></tr>");
-    }
-
-    var innereTabelle = popupcontent.join("");
-    
-    layer.bindPopup(
-        "<h1>" +feature.properties.name +"</h1>"
-        +"<table>" +innereTabelle + "</table>"
-        +"<p class='popupText'>Fehlende oder falsche Angaben? Trage Daten für diesen Ort <a href='http://openstreetmap.org/" +feature.id  +"'> auf Opentreetmap</a> ein! <br>Die Daten werden regelmäßig abgeglichen.</p>"
-    );
-};
+    var popupcontent = [];
+   
+    var popupcontent = [];
+       for (var prop in feature.properties) {
+         if (prop == "@id" || prop == "shop"){
+           //do nothing
+           }
+         else if (prop == "website" || prop == "contact:website"){
+             popupcontent.push("<tr><td>" +prop.replace("website","Internetseite").replace("contact:","") + ": </td><td>" + "<a link href='" + feature.properties[prop] + "' target='_blank'>" +feature.properties[prop] +"</a></td></tr>");
+           }
+   
+           else {
+             popupcontent.push("<tr><td>" +prop.replace(":", " ").replace("addr ", "").replace("name", "Name").replace("opening_hours", "Öffnungszeiten").replace("city", "Stadt").replace("housenumber", "Hausnummer").replace("phone", "Telefon").replace("operator", "Betreiber").replace("postcode", "Postleitzahl").replace("street", "Straße").replace("organic", "Biologisch") + ": </td><td>" + feature.properties[prop].replace(";", ", ").replace("yes", "ja").replace("only", "nur") + "</td></tr>");
+           }
+       }
+   
+   
+       var innereTabelle = popupcontent.join("");
+   
+       layer.bindPopup(
+           "<h1>" +feature.properties.name +"</h1>"
+           +"<table>" +innereTabelle + "</table>"
+           +"<p class='popupText'>Fehlende oder falsche Angaben? Trage Daten für diesen Ort <a href='http://openstreetmap.org/" +feature.id  +"'> auf Opentreetmap</a> ein! <br>Die Daten werden regelmäßig abgeglichen.</p>"
+       );
+   };
 
 L.geoJson(karlsruhe, {
     onEachFeature: jederPunkt
