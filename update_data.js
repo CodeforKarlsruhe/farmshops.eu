b65d3a1b3259c4d3f48c98374e298862017e6eca
Stefan Grotz <stefan.grotz@1und1.de>
2018-04-20 16:16:49 +0200

    update_data umstrukturiert - vorbereitung auf verschiedene Regionen

--- update_data.js
+++ update_data.js
@@ -2,23 +2,24 @@ const query_overpass = require("query-overpass");
 const fs = require("fs")
 
 let filename = "data/karlsruhe.js"
-
+let bbox = "48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125";
+console.log('starting query for ' +filename +' bbox: ' +bbox)
 let query = ` 
   [out:json][timeout:25];
   // gather results
   (
   // query part for: “vending=milk”
-  node["vending"="milk"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  way["vending"="milk"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  relation["vending"="milk"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
+  node["vending"="milk"](${bbox});
+  way["vending"="milk"](${bbox});
+  relation["vending"="milk"](${bbox});
   // query part for: “shop=farm”
-  node["shop"="farm"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  way["shop"="farm"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  relation["shop"="farm"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
+  node["shop"="farm"](${bbox});
+  way["shop"="farm"](${bbox});
+  relation["shop"="farm"](${bbox});
   // query part for: “vending=food”
-  node["vending"="food"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  way["vending"="food"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
-  relation["vending"="food"](48.52388120259336,7.6519775390625,49.66407240384585,9.53887939453125);
+  node["vending"="food"](${bbox});
+  way["vending"="food"](${bbox});
+  relation["vending"="food"](${bbox});
   );
   // print results
   out body;
