5cba45617025ba00dc9f89e591d9aadd28e4afce
Stefan Grotz <stefan.grotz@1und1.de>
2018-06-11 17:39:23 +0200

    Datenupdate (7303)

--- update.sh
+++ update.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 echo $1
 if [[ `git status --porcelain` ]]; then
-git pull origin master
-  echo 'changes'
+  echo 'changes detected'
+  git pull origin master
+  node update_data.js
+  echo 'finished'
 else
-
-  echo 'no changes'
+  echo 'no changes detected'
+  node update_data.js
+  echo 'finished'
 fi
+echo 'loop finished'
+
+git add data/*
+git commit -m "Datenupdate"
+git push origin master
 
 
 
