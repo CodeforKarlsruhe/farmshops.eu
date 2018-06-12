36545d280497b7307a0ad1fbe21d3559f1352b89
Stefan Grotz <stefan.grotz@1und1.de>
2018-06-12 11:39:42 +0200

    Script verbessert

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $1
+git pull origin master
 if [[ `git status --porcelain` ]]; then
   echo 'changes detected'
   git pull origin master
@@ -11,10 +12,8 @@ fi
 echo 'loop finished'
 
 number=$(find data -type f | wc -l)
-
+let number-=2
+echo "Anzahl der Nodes: $number"
 git add data/*
-git commit -m "Datenupdate ($number)"
+git commit -m "Datenupdate $number"
 git push origin master
-
-
-
