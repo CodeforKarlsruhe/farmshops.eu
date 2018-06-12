21b60984fec6b1e0e5d31e5a485edc3ab72b4e2f
Stefan Grotz <stefan.grotz@1und1.de>
2018-06-12 10:46:24 +0200

    Bash Script weiter verbessert

--- update.sh
+++ update.sh
@@ -4,16 +4,16 @@ if [[ `git status --porcelain` ]]; then
   echo 'changes detected'
   git pull origin master
   node update_data.js
-  echo 'finished'
 else
   echo 'no changes detected'
   node update_data.js
-  echo 'finished'
 fi
 echo 'loop finished'
 
+number=$(find data -type f | wc -l)
+
 git add data/*
-git commit -m "Datenupdate"
+git commit -m "Datenupdate ($number)"
 git push origin master
 
 
