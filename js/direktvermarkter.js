8f288ce62631ce52b7f12b61cd2852160a3db3a1
Stefan Grotz <stefan.grotz@1und1.de>
2018-09-11 20:48:52 +0200

    weitere css optimierungen für cluster

--- js/direktvermarkter.js
+++ js/direktvermarkter.js
@@ -150,7 +150,7 @@ var markers = L.markerClusterGroup({
             }
             
             if (map.getZoom() >= 10){
-                returnWert = markers.length +"<div style='padding-top:2px;>" +farmsAreInCluster(farmsInCluster) +marketsAreInCluster(marketsInCluster) +machinesAreInCluster (machinesInCluster) +"</div>";
+                returnWert = markers.length +"<div style='padding-top:2px;'>" +farmsAreInCluster(farmsInCluster) +marketsAreInCluster(marketsInCluster) +machinesAreInCluster (machinesInCluster) +"</div>";
             }
             else{
                 returnWert = markers.length;
